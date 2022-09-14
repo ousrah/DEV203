14a2b5b082a08359c15076b96bc41782e4885351
rahmouni oussama <ousrah@hotmail.com>
2022-09-14 09:10:36 +0100

    exercice select

---
+++ bases de données/1 - test de posionnement/select.sql
@@ -0,0 +1,15 @@
+#A.	La liste des bien de type ‘villa’
+
+
+
+#B.	La liste des appartements qui se trouve à Tétouan
+
+#C.	La liste des appartements loués par M. Marchoud Ali
+
+#D.	Le nombre des appartements loués dans le mois en cours
+
+#E.	Les appartements disponibles actuellement à Martil dont le loyer est inférieur à 2000 DH triés du moins chère au plus chère
+
+#F.	La liste des biens qui n’ont jamais été loués
+
+#G.	La somme des loyers du mois en cours
